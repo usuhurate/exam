ae09ec00e352b972449c55cc5d8a63b66563b78a
usuhurate <usuhu52@gmail.com>
2021-11-24 12:40:00 +0800

    finished ushuu

--- datatypes/index.js
+++ datatypes/index.js
@@ -2,9 +2,24 @@ const string = 'apple, banana, kiwi';
 
 // 1. Өгсөн string - ийн бүх үсгийг том болго: 'APPLE, BANANA, KIWI'
 // -- Энд кодоо бичнэ үү!
+function toUpperCase(string) {
+    let result = '';
+    for (let i = 0; i < string.length; i++) {
+        if(string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+            result += String.fromCharCode(string.charCodeAt(i) - 32);
+        }else {
+            result += string.charAt(i);
+        }
+    }
+    return result;
+}
+
+// console.log(toUpperCase('hello')) // HELLO
+// console.log(toUpperCase('hello world')) // HELLOWORLD
 
 // 2. Өгсөн string - ийг дараах хэлбэртэй болго: 'Apple; Banana; Kiwi'
 // -- Энд кодоо бичнэ үү!
+
 
 const array = ['d', 'e', 'c', 'a', 'b'];
 // 3. Өгсөн array - ыг үсгийн дарааллаар эрэмблэ.
@@ -16,6 +31,22 @@ const array = ['d', 'e', 'c', 'a', 'b'];
 
 // 5. Өгсөн array - ын эхний элемэнтийг устага
 // -- Энд кодоо бичнэ үү!
+// function cutFromArray(arr, from, to) {
+//     let myArray = []
+
+//     for (i = 0; i < array.length; i++) {
+//         for (let j = from; j < to; i++) {
+//             myArray.push(arr[j])
+//         }
+//     }
+//     return 
+// }
+
+
+// function cutFromArray
+// const arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+// console.log(cutFromArray(arr, 2, 6));
+
 
 // 6. Өгсөн array - ын сүүлийн элемэнтийг устага
 // -- Энд кодоо бичнэ үү!
